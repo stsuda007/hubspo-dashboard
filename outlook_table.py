0b06c03ca6dff1418505b4dba9018e91a30dac19
stsuda007 <stsuda.indee@gmail.com>
2025-08-12 17:18:35 +0900

    Update outlook_table.py

--- outlook_table.py
+++ outlook_table.py
@@ -118,9 +118,9 @@ def display_pipeline_projects_table(df):
         '納品予定日_dt',
         '見込売上額（円）',
         '受注金額（円）',
-        'フェーズ'
+        'フェーズ',
         #'見込売上額', # 元の金額列
-        #'受注金額', # 元の金額列
+        '受注金額' # 元の金額列
     ]
     display_df = display_df[cols_to_display]
 
